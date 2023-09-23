2db822d2f20501a74ca27a8ff92c728cb4eab129
Bryan-an <bryanandagoya@gmail.com>
2023-09-23 11:12:04 -0500

    featured component

--- src/app/_components/featured/featured.tsx
+++ src/app/_components/featured/featured.tsx
@@ -2,6 +2,7 @@
 
 import ChevronLeftIcon from '@/icons/chevron-left';
 import ChevronRightIcon from '@/icons/chevron-right';
+import PointFilledIcon from '@/icons/point-filled';
 import { useState } from 'react';
 
 const sliders = [
@@ -19,19 +20,43 @@ const sliders = [
 export const FeaturedComponent = () => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
+  const handlePrevSlidePress = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? sliders.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const handleNextSlidePress = () => {
+    const isLastSlide = currentIndex === sliders.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
   return (
-    <div className="max-w-[1520px] h-[500px] w-full p-4 relative">
-      <img
-        src={sliders[currentIndex].url}
-        className="w-full h-full rounded-2xl object-cover duration-500"
-        alt="Featured meals images"
-      />
-      <button className="absolute top-[50%] -translate-x-0 -translate-y-[50%] left-6 text-2xl rounded-full p-2 bg-orange-700 text-white w-10 h-10 flex justify-center items-center hover:bg-orange-600">
+    <div className="max-w-[1520px] h-[500px] w-full p-4 relative group">
+      <div
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+        style={{ backgroundImage: `url(${sliders[currentIndex].url})` }}
+      ></div>
+      <button
+        className="hidden absolute top-[50%] -translate-x-0 -translate-y-[50%] left-6 text-2xl rounded-full p-2 bg-orange-700 text-white w-10 h-10 group-hover:flex justify-center items-center hover:bg-orange-600"
+        onClick={handlePrevSlidePress}
+      >
         <ChevronLeftIcon />
       </button>
-      <button className="absolute top-[50%] -translate-x-0 -translate-y-[50%] right-6 text-2xl rounded-full p-2 bg-orange-700 text-white w-10 h-10 flex justify-center items-center hover:bg-orange-600">
+      <button
+        className="hidden absolute top-[50%] -translate-x-0 -translate-y-[50%] right-6 text-2xl rounded-full p-2 bg-orange-700 text-white w-10 h-10 group-hover:flex justify-center items-center hover:bg-orange-600"
+        onClick={handleNextSlidePress}
+      >
         <ChevronRightIcon />
       </button>
+      <div className="flex top-4 justify-center py-2 gap-3">
+        {sliders.map((slide, index) => (
+          <button key={slide.url} onClick={() => setCurrentIndex(index)}>
+            <PointFilledIcon />
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
